f2de18d6c9067e65a7f54cfe2889efd7626b2624
Mario de Frutos <ethervoid@gmail.com>
2014-12-02 08:36:24 +0100

    Added checks to avoid create partition and filesystem if they exists

--- provision.sh
+++ provision.sh
@@ -3,6 +3,9 @@
 export LFS=/mnt/lfs
 
 # Check if partitions exists if not create
+if [ `fdisk -l /dev/sdb | grep /dev/sdb | sed '/^Disk /d' | sed 's/^\([^ ]*\)  *.*/\1/' | wc -l` = 0 ]
+then
+    echo "Creating partitions for LFS..."
 sfdisk /dev/sdb << EOF
 0,20,L,*
 ,,E
@@ -10,12 +13,23 @@ sfdisk /dev/sdb << EOF
 ;
 ,,L
 EOF
+    echo "Partitions created"
 
-# Create filesystem for LFS partition
-mkfs -v -t ext2 /dev/sdb1
-mkfs -v -t ext4 /dev/sdb5
+    # Create filesystem for LFS partition
+    echo "Creating filesystem for LFS..."
+    mkfs -v -t ext2 /dev/sdb1
+    mkfs -v -t ext4 /dev/sdb5
+    mkdir -pv $LFS
+    mkdir -pv $LFS/boot
+    echo "Filesystem created"
+else
+    echo "[Skipping] The LFS filesystem is already created"
+fi
 
-mkdir -pv $LFS
-mkdir -pv $LFS/boot
-mount -v -t ext2 /dev/sdb1 $LFS/boot
-mount -v -t ext4 /dev/sdb5 $LFS
+if [ `mount -l | grep /dev/sdb | wc -l` = 0 ]
+then
+    mount -v -t ext2 /dev/sdb1 $LFS/boot
+    mount -v -t ext4 /dev/sdb5 $LFS
+else
+    echo "[Skipping] LFS partitions are already mounted"
+fi
